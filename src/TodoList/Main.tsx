0d5425a6e01c5e687de44f01c399dfa1a898a0f8
代云舟 <daiyunz@chanjet.com>
2020-12-13 15:03:21 +0800

    add

--- src/TodoList/Main.tsx
+++ src/TodoList/Main.tsx
@@ -1,17 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { observer } from "../modules/mobx-react/mobxreact.cjs.development";
-import TodoListStore from './Store';
-import { observable } from "../modules/mobx/lib/mobx";
+import TodoListStore from "./Store";
+import { observable, autorun,computed } from "../modules/mobx/lib/mobx";
+import { threadId } from "worker_threads";
 // import { observable } from 'mobx';
 
 @observer
 export default class TodoList extends Component {
-  @observable haha = 1;
-  render(){
-    return (
-      <div>
-        {TodoListStore.pageTitle}
-      </div>
-    )
+  @observable private data = { height: 20, width: 10 };
+  @computed get count(){
+    return this.data.height + this.data.width;
+  }
+  componentDidMount(){
+    autorun(()=>{
+      console.log(this.data.height);
+    })
+  }
+  onclick = ()=>{
+    this.data.height ++ ;
+  }
+
+  render() {
+  return <div onClick={this.onclick}>{this.data.height}-count:{this.count}</div>;
   }
 }
--- src/TodoList/Store.ts
+++ src/TodoList/Store.ts
@@ -1,42 +1,49 @@
 // import {observable, computed,configure,action} from 'mobx';
-import { observable, computed,configure,action} from "../modules/mobx/lib/mobx";
+import {
+  observable,
+  computed,
+  configure,
+  action,
+} from "../modules/mobx/lib/mobx";
 
-import { enableLogging } from 'mobx-logger';
+import { enableLogging } from "mobx-logger";
 
 // mobx 日志,只有本地开发环境会有日志
 enableLogging({
-    predicate: () => true,
-    action: true,
-    reaction: true,
-    transaction: true,
-    compute: true
+  predicate: () => true,
+  action: true,
+  reaction: true,
+  transaction: true,
+  compute: true,
 });
 
 // 不允许在动作外部修改状态,修改store必须使用action
 configure({
-    enforceActions: 'observed'
+  enforceActions: "observed",
 });
 
 class TodoListStore {
-    constructor(){
-        this.init();
-    }
-    @observable public title:string = 'haha';
+  @observable public data = { height: 20, width: 10 };
+  
 
-    @computed get pageTitle () {
-        return this.title || '没有标题';
-    }
 
-    init (){
-        setTimeout(() => {
-           this.update();
-        }, 1000);
-    }
 
-    @action
-    update(){
-        this.title = '111111';
-    }
+  
+  // @observable public title:string = 'haha';
 
+  // @computed get pageTitle () {
+  //     return this.title || '没有标题';
+  // }
+
+  // init (){
+  //     setTimeout(() => {
+  //        this.update();
+  //     }, 1000);
+  // }
+
+  // @action
+  // update(){
+  //     this.title = '111111';
+  // }
 }
 export default new TodoListStore();
